23295fd94d0c76030924f90a446fe0341d0c3978
ChetanDigholeLY <chetan.d@ly.design>
2023-08-01 14:42:00 +0530

    implemented dynamic navBar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,34 @@
-import { Component , Input, EventEmitter, Output} from '@angular/core';
+import { Component , Input, EventEmitter, Output, OnInit} from '@angular/core';
 import { SidebarService } from 'src/app/services/sidebar.service';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+const routeNameMap: { [key: string]: String[]} = {
+  '/worldmap':["World Map"],
+  '/guidance': ["Automated Guidance"],
+  '/sensors/livedatafeed': ["Sensor Data","Live Data Feed"],
+  '/performance': ["Performance", "Electric Power Performance"]
+}
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  currentRoute: String[] = routeNameMap['/guidance']
 
-  constructor(private sideBarService: SidebarService) {}
+  constructor(private route: ActivatedRoute, private router: Router, private sideBarService: SidebarService) {
+  }
+  
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentRoute = routeNameMap[event.url]
+      console.log(this.currentRoute)
+    })
+  }
 
   @Input() dataFromAppComp: boolean | undefined;
 
