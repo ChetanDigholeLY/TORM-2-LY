156a43a5251a2480bf4d805e6b902671d3f48f4d
Shubham-ly <shubham.m@ly.design>
2023-08-09 13:01:27 +0530

    Fixed previous date being selected on action button click

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,53 +1,51 @@
-import { KeyValue } from "@angular/common";
+import { KeyValue } from '@angular/common';
 import {
   Component,
   EventEmitter,
   Input,
   Output,
   ViewChild,
-} from "@angular/core";
-import dayjs from "dayjs";
+} from '@angular/core';
+import dayjs from 'dayjs';
 
-const dateFormatter = new Intl.DateTimeFormat("en-GB", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-  hour: "2-digit",
-  minute: "2-digit",
+const dateFormatter = new Intl.DateTimeFormat('en-GB', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
   hour12: true,
 });
 
 function customDateFormatter(date: Date | number | any): String {
-  return dateFormatter.format(date)
-    .replaceAll("/", "-")
-    .replaceAll(",", " ");
+  return dateFormatter.format(date).replaceAll('/', '-').replaceAll(',', ' ');
 }
 
 @Component({
-  selector: "date-picker",
-  templateUrl: "./date-picker.component.html",
-  styleUrls: ["./date-picker.component.css"],
+  selector: 'date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css'],
   host: {
-    "(document:click)": "onDocumentClick($event)",
+    '(document:click)': 'onDocumentClick($event)',
   },
 })
 export class DatePickerComponent {
   selectedFrom = customDateFormatter(new Date());
   selectedTo = customDateFormatter(new Date(new Date().getDate() + 1));
 
-  @ViewChild("datepicker")
+  @ViewChild('datepicker')
   datePickerRef: any;
-  @ViewChild("container")
+  @ViewChild('container')
   container: any;
 
   @Input()
   showDatePicker = false;
 
   @Input()
-  startDate = { label: "", value: dayjs() };
+  startDate = { label: '', value: dayjs() };
 
   @Input()
-  endDate = { label: "", value: dayjs() };
+  endDate = { label: '', value: dayjs() };
 
   @Output()
   closeDatePickerEvent = new EventEmitter<any>();
@@ -59,61 +57,59 @@ export class DatePickerComponent {
   }
 
   ranges: any = {
-    "Last Hour": [dayjs().subtract(1, "hour"), dayjs()],
-    "Last 3 hours": [dayjs().subtract(3, "hour"), dayjs()],
-    "Last 12 hours": [dayjs().subtract(12, "hour"), dayjs()],
-    "Last 24 hours": [dayjs().subtract(24, "hour"), dayjs()],
-    "Last week": [dayjs().subtract(6, "days"), dayjs()],
-    "Last Month": [
-      dayjs().subtract(1, "month").startOf("month"),
-      dayjs().subtract(1, "month").endOf("month"),
+    'Last Hour': [dayjs().subtract(1, 'hour'), dayjs()],
+    'Last 3 hours': [dayjs().subtract(3, 'hour'), dayjs()],
+    'Last 12 hours': [dayjs().subtract(12, 'hour'), dayjs()],
+    'Last 24 hours': [dayjs().subtract(24, 'hour'), dayjs()],
+    'Last week': [dayjs().subtract(6, 'days'), dayjs()],
+    'Last Month': [
+      dayjs().subtract(1, 'month').startOf('month'),
+      dayjs().subtract(1, 'month').endOf('month'),
     ],
-    "Last 3 Month": [
-      dayjs().subtract(3, "month").startOf("month"),
-      dayjs().subtract(1, "month").endOf("month"),
+    'Last 3 Month': [
+      dayjs().subtract(3, 'month').startOf('month'),
+      dayjs().subtract(1, 'month').endOf('month'),
     ],
-    "Last Year": [
-      dayjs().subtract(1, "year").startOf("month"),
-      dayjs(),
-    ],
-    "All": [dayjs(), dayjs()],
+    'Last Year': [dayjs().subtract(1, 'year').startOf('month'), dayjs()],
+    All: [dayjs(), dayjs()],
   };
 
   originalOrder = (
     _a: KeyValue<number, string>,
-    _b: KeyValue<number, string>,
+    _b: KeyValue<number, string>
   ): number => {
     return 0;
   };
 
   updateDateRange() {
-    let dates = this.datePickerRef.chosenLabel.split("-").map((date: string) =>
-      date.trim()
-    );
+    let dates = this.datePickerRef.chosenLabel
+      .split('-')
+      .map((date: string) => date.trim());
     this.selectedFrom = customDateFormatter(new Date(dates[0]));
     this.selectedTo = customDateFormatter(new Date(dates[1]));
   }
 
   onRangeButtonClick(event: any, key: any) {
+    let dates = this.datePickerRef.chosenLabel
+      .split('-')
+      .map((date: string) => date.trim());
+
     this.datePickerRef.setStartDate(event[0]);
     this.datePickerRef.setEndDate(event[1]);
     this.updateDateRange();
     this.datePickerRef.updateCalendars();
     this.closeDatePickerEvent.emit({
-      range: [
-        this.datePickerRef.startDate,
-        this.datePickerRef.endDate,
-      ],
+      range: [new Date(dates[0]), new Date(dates[1])],
       predefinedValue: key,
     });
   }
 
   onActionButtonClick() {
+    let dates = this.datePickerRef.chosenLabel
+      .split('-')
+      .map((date: string) => date.trim());
     this.closeDatePickerEvent.emit({
-      range: [
-        this.datePickerRef.startDate,
-        this.datePickerRef.endDate,
-      ],
+      range: [new Date(dates[0]), new Date(dates[1])],
       predefinedValue: '',
     });
   }
