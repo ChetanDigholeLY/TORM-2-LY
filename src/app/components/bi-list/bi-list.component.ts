588f16b25a8ef50091ab0e78f277d668b8d44582
ChetanDigholeLY <chetan.d@ly.design>
2023-09-13 19:22:50 +0530

    updtd bi with edit reprt and flder name

--- src/app/components/bi-list/bi-list.component.ts
+++ src/app/components/bi-list/bi-list.component.ts
@@ -10,13 +10,12 @@ export class BiListComponent {
 
   constructor(private sideBarService: SidebarService) {}
 
-  @Input() data: any;
+  @Input() data: any; //api in json
   @Input() biReportMainArrData: any;
 
   closeBody: boolean = false;
   closeBodyFunc () {
     this.closeBody = !this.closeBody;
-    // console.log(this.data);
   }
 
   
@@ -25,5 +24,48 @@ export class BiListComponent {
   expandAll = this.sideBarService.ExpandAllCollapsible$.subscribe((val) => {this.expandAllRow = val; this.closeBody = val})
   //expandAll ends
   
+
+  //edit header text starts
+  editHeaderText: boolean = false;
+  editHeaderTextFunc(){
+    this.editHeaderText = !this.editHeaderText
+    this.biReportEditedHeader = this.data.title
+  }
+
+  biReportEditedHeader: string = '';
+  biReportEditedHeaderFunc(){
+    this.data.title = this.biReportEditedHeader
+    this.editHeaderText = !this.editHeaderText
+  }
+
+  //edit header text ends
+
+  //edit report section starts
+
+  reportDetail = [
+    {
+      name: 'Report',
+      workSpace: '9871324SDAS',
+      reportId: '4SDAS',
+      ShowOnVessel: true,
+      ShowOnShore: true,
+      isEditReport: false
+    },
+    {
+      name: 'Report',
+      workSpace: '9871324SDAS',
+      reportId: '4SDAS',
+      ShowOnVessel: true,
+      ShowOnShore: true,
+      isEditReport: false
+    },
+  ]
+
+  editReport: boolean = false;
+  editReportFunc(val : number){
+    this.reportDetail[val].isEditReport = !this.reportDetail[val].isEditReport
+  }
+
+  //edit report section ends
 
 }
